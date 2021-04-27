7b96177c64563b54efc2407ec2897984bc1624de
Nicholas Jacob <nicholas.jacob@guitarcenter.com>
2021-04-27 14:45:33 -0700

    Handling form submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -35,8 +35,21 @@ const ExpenseForm = () => {
     //     enteredDate: event.target.value
     // })
   };
+
+  const submitHandler = (event) => {
+      event.preventDefault();
+
+      const expenseData = {
+          title: enteredTitle,
+          amount: enteredAmount,
+          date: new Date(enteredDate)
+      };
+
+      console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__controls">
           <label>Title</label>
